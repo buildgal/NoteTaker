b581a65613181a3c35d12bd26b60f2a2f4beee90
buildgal <gjaquelin7@outlook.com>
2020-12-16 22:38:44 -0600

    adding the get HTML route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,5 +1,9 @@
 module.exports = function (app){
     app.post("/notes", function(req,res){
-        res.sendFile(path.join(__dirname, "../public/notes.html"));
+        res.sendFile(path.join(__dirname, "../public/db.json"));
+    })
+
+    app.get("/notes", function(req,res){
+        res.readFile(path.join(__dirname, "../public/db.json"));
     })
 }
