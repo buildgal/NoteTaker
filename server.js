c15a8d7725f21ac4d79e53cff63141f5ade37ca3
buildgal <gjaquelin7@outlook.com>
2020-12-16 22:07:36 -0600

    adding server code

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 //setting up the server in express 
 const express = require("express");
-const path = require("path");
+
 
 let app= express();
-let PORT = 3000;
+var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
+  
