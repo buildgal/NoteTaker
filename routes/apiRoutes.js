887bd29e67c3c8c80ad79699b22a3a14e7f3cc39
buildgal <gjaquelin7@outlook.com>
2020-12-16 22:37:00 -0600

    updating the post

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,10 +3,12 @@ let notesData = "../db/db.json";
 module.exports = function(app){
 
     app.get("/api/notes", function(req, res) {
+     
         res.json(notesData);
       });
 
       app.post("/api/notes", function(req, res) {
+        notesData.push(req.body);
         res.json(noteData);
       });
     
